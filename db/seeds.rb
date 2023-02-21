5b47f5298acfdc74ddd57ee7f96fdc7280024b92
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-21 19:46:42 +0200

    seed for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user1 = User.create!(name: 'kareem')
+category1 = Category.create!(user: user1, name: 'entertainment', icon: 'url here')
+payment1 = Payment.create!(user: user1, name: 'buy product from amazon', amount: 270)
+category_payment = CategoryPayment.create!(category: category1, payment: payment1)
