4484e6054eaea318d541487b6e3ddc7407a22f68
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-24 21:11:01 +0200

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :categories
   has_many :payments
+
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
