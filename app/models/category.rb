dd6354cb93bf671ea7d24c7531eee0be1d4fa29d
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-24 20:29:02 +0200

    Add validations to category and payment models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :category_payments
   has_many :payments, through: :category_payments
+
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :icon, presence: true, numericality: { greater_than_or_equal: 0 }
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,8 @@ class Payment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :category_payments
   has_and_belongs_to_many :categories, through: :category_payments
+
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
