f0491b265ed91e49e46ca7febc0a0fd695786226
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-24 17:13:12 +0200

    Add total amout feature in payments view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,28 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @page_name = 'Categories'
+    @categories = current_user.categories
+  end
+    
+  def new 
+    @page_name = 'Create Category'
+    @category = Category.new
+  end
+
+  def create 
+    @category = Category.new(category_params.merge(user: current_user))
+
+    if @category.save
+      redirect_to categories_path
+      flash[:notice] = 'Category was successfully added.'
+    else
+      flash[:alert] = "Couldn't add this Category to your categories list."
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
