e12458263e0ebeab665c3c4ccfb9511619211b9b
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-24 17:12:37 +0200

    Create add new payment form

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,10 @@ user1 = User.create!(name: 'kareem')
 category1 = Category.create!(user: user1, name: 'entertainment', icon: 'url here')
 payment1 = Payment.create!(author: user1, name: 'buy product from amazon', amount: 270)
 payment_for_category = CategoryPayment.create!(category: category1, payment: payment1)
+
+pay1 = Payment.create!(author: user1, name: 'buy product from amazon', amount: 270)
+pay2 = Payment.create!(author: user1, name: 'food', amount: 30)
+pay3 = Payment.create!(author: user1, name: 'new camera', amount: 300)
+
+
+CategoryPayment.create!(category: cat, payment: pay3)
