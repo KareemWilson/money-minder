3976da8edcdb993a3473ceedecdd11cdfed22a99
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-24 17:07:32 +0200

    Adjust Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
-  authenticated :user do
-    root 'categories#index', as: :authenticated_root
-    resources :categories do
-      resources :payments  
+  devise_scope :user do
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'splash#index', as: :unauthenticated_root
     end
   end
-  
-  unauthenticated do
-    root 'splash#index', as: :unauthenticated_root
+
+  resources :categories do
+    resources :payments  
   end
 end
